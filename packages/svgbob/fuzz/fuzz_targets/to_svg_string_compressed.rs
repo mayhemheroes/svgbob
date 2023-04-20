caa8ec78a99ef2c2ef18cedb318e778573fcb039
Dylan <57853417+4812571@users.noreply.github.com>
2023-04-20 01:21:46 -0400

    fuzz

---
+++ packages/svgbob/fuzz/fuzz_targets/to_svg_string_compressed.rs
@@ -0,0 +1,9 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use svgbob::to_svg_string_compressed;
+
+fuzz_target!(|data: &[u8]| {
+    let input = String::from_utf8_loss(data);
+    let _ = to_svg_string_compressed(&input);
+});
